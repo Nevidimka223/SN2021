2f08008b2a5b4e9566432011b695e8a86186109b
Vinokur Vova <vinokur-vova@i.ua>
2021-12-11 20:25:57 +0200

    add eslint and husky

---
+++ api/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const config = require('./config/config');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(config.port, () => {
+  console.log(`Example app listening at http://localhost:${config.port}`);
+});
